b6e6683f0394d71b26d5af79a9efb84fd5eb1ddd
Tom Hinton <tomjhinton@gmail.com>
2019-03-31 21:56:39 +0100

    t rotate

--- app.js
+++ app.js
@@ -10,37 +10,28 @@ document.addEventListener('DOMContentLoaded', () => {
   //  up: index-width - if(index-width >= 0)
 
   const board = document.querySelector('.board')
-  const iSelect = document.querySelectorAll('.board > .i')
-  const lSelect = document.querySelectorAll('.board > .l')
-  const jSelect = document.querySelectorAll('.board > .j')
-  const oSelect = document.querySelectorAll('.board > .o')
-  const sSelect = document.querySelectorAll('.board > .s')
-  const tSelect = document.querySelectorAll('.board > .t')
-  const zSelect = document.querySelectorAll('.board > .z')
+  //const iSelect = document.querySelectorAll('.board > .i')
+  //const lSelect = document.querySelectorAll('.board > .l')
+  //const jSelect = document.querySelectorAll('.board > .j')
+  //const oSelect = document.querySelectorAll('.board > .o')
+  //const sSelect = document.querySelectorAll('.board > .s')
+  //const tSelect = document.querySelectorAll('.board > .t')
+  //const zSelect = document.querySelectorAll('.board > .z')
 
-function assignClass() {
 
-}
-
-  let random = Math.floor(Math.random()* 7)
+  const random = Math.floor(Math.random()* 7)
   console.log(random)
 
+  const scoreDiv = document.querySelector('#score')
 
 
-  function newPiece(){
-    random = Math.floor(Math.random()* 7)
-    choice = random
-    inPlay = pieces[choice]
-    inPlayColor = piecesColor[choice]
-    console.log('new pieces')
-  }
-
+  let score = 0
   const width = 9
   const square =[]
   const squareMap = []
 
   function makeBoard(){
-    for(let i=0;i <81; i++){
+    for(let i=0;i <99; i++){
       const div = document.createElement('div')
       board.appendChild(div)
       square.push(0)
@@ -49,10 +40,15 @@ function assignClass() {
 
   }
 
-makeBoard()
 
 
-square.forEach((element)=>{
+
+
+
+  makeBoard()
+
+
+  square.forEach((element)=>{
     if(!squareMap.length || squareMap[squareMap.length-1].length === width)
       squareMap.push([])
 
@@ -61,9 +57,9 @@ square.forEach((element)=>{
 
 
 
-function clearBoard(){
+  function clearBoard(){
     squareMap.forEach(row => row.fill(0))
-}
+  }
 
 
 
@@ -74,13 +70,13 @@ function clearBoard(){
 
 
 
-  const i = [3,4,5,6]
-  const l = [3,12,13,14]
-  const j = [5,12,13,14]
-  const o = [3,4,13,14]
-  const s = [4,5,12,13]
-  const t = [12,13,14,4]
-  const z = [4,5,12,13]
+  //const i = [3,4,5,6]
+  //const l = [3,12,13,14]
+  //const j = [5,12,13,14]
+  //const o = [3,4,13,14]
+  //const s = [4,5,12,13]
+  //const t = [12,13,14,4]
+  //const z = [4,5,12,13]
 
 
 
@@ -90,12 +86,12 @@ function clearBoard(){
   const iMap = [0,0,1,1,1,1,0,0,0]
 
 
-  const lMap = [3,12,13,14]
-  const jMap = [5,12,13,14]
-  const oMap = [3,4,13,14]
-  const sMap = [4,5,12,13]
-  const tMap = [12,13,14,4]
-  const zMap = [4,5,12,13]
+  //const lMap = [3,12,13,14]
+  //const jMap = [5,12,13,14]
+  //const oMap = [3,4,13,14]
+  //const sMap = [4,5,12,13]
+  //const tMap = [12,13,14,4]
+  //const zMap = [4,5,12,13]
 
 
 
@@ -104,101 +100,7 @@ function clearBoard(){
 
 
 
-  function moveShapesDown(){
-    var canMove = true
 
-    for( var y = 0; y < squareMap.length; y++) {
-
-      for (var x=0; x<squareMap[y].length; x++) {
-
-  		if(squareMap[y][x] > 0 && squareMap[y][x] < 10){
-  				if(y === squareMap.length-1 || squareMap[y+1][x] >10 ) {
-  					canMove = false;
-  					freeze();
-  				}
-
-  			}
-  		}
-
-  	}
-  	if(canMove) {
-
-  		for(var y=squareMap.length-1;  y>=0;  y-- ){
-
-  		for (var x=0; x<squareMap[y].length; x++) {
-  			if(squareMap[y][x] > 0 && squareMap[y][x] < 10){
-  				squareMap[y+1][x] = squareMap[y][x];
-  				squareMap[y][x] =0;
-
-  			}
-
-
-  		}
-
-  	}
-
-
-    }
-
-  }
-
-
-
-  function freeze(){
-  	for( var y = 0; y < squareMap.length; y++) {
-
-  		for (var x=0; x<squareMap[y].length; x++) {
-
-  			if(squareMap[y][x] > 0 && squareMap[y][x] < 10){
-  				squareMap[y][x] = squareMap[y][x] + 10
-  			}
-
-  		}
-  	}
-  	//	checkLines();
-
-  		var ran = Math.floor(Math.random()*7)
-
-  		if (ran === 0){
-
-  		squareMap[0] =[0,0,0,2,2,0,0,0]
-
-  		squareMap[1]= [0,0,0,2,2,0,0,0]
-
-
-  	}else if (ran === 1){
-
-  		squareMap[0] =[0,0,0,0,0,0,0,0]
-
-  		squareMap[1]= [0,0,4,4,4,4,0,0]
-
-  		}else if (ran === 2){
-
-  		squareMap[0] =[0,0,0,3,0,0,0,0]
-
-  		squareMap[1]= [0,0,0,3,3,3,0,0]
-  	}else if (ran === 3){
-
-    squareMap[0] =[0,0,0,0,0,5,0,0]
-
-    squareMap[1]= [0,0,0,5,5,5,0,0]
-  }else if (ran === 4){
-
-  squareMap[0] =[0,0,0,0,6,6,0,0]
-
-  squareMap[1]= [0,0,0,6,6,0,0,0]
-}else if (ran === 5){
-
-squareMap[0] =[0,0,0,7,0,0,0,0]
-
-squareMap[1]= [0,0,7,7,7,0,0,0]
-}else if (ran === 6){
-
-squareMap[0] =[0,0,0,0,0,8,0,0]
-
-squareMap[1]= [0,0,0,8,8,8,0,0]
-}
-  }
 
 
 
@@ -207,7 +109,7 @@ squareMap[1]= [0,0,0,8,8,8,0,0]
 
 
   function drawWorld(a,b){
-
+    scoreDiv.innerText = `${score}`
     squareMap[0].forEach(function (element, index)  {
       if(element === a){
         squares[index].removeAttribute('class')
@@ -262,19 +164,399 @@ squareMap[1]= [0,0,0,8,8,8,0,0]
         squares[index+72].classList.add(b)
       }
     })
+    squareMap[9].forEach(function (element, index)  {
+      if(element === a){
+        squares[index+81].removeAttribute('class')
+        squares[index+81].classList.add(b)
+      }
+    })
+    squareMap[10].forEach(function (element, index)  {
+      if(element === a){
+        squares[index+90].removeAttribute('class')
+        squares[index+90].classList.add(b)
+      }
+    })
 
 
   }
 
 
+
+  function moveShapesDown(){
+    let canMove = true
+
+    for( let y = 0; y < squareMap.length; y++) {
+
+      for (let x=0; x<squareMap[y].length; x++) {
+
+        if(squareMap[y][x] > 0 && squareMap[y][x] < 10){
+          if(y === squareMap.length-1 || squareMap[y+1][x] >10 ) {
+            canMove = false
+            freeze()
+          }
+
+        }
+      }
+
+    }
+    if(canMove) {
+
+      for(let y=squareMap.length-1;  y>=0;  y-- ){
+
+        for (let x=0; x<squareMap[y].length; x++) {
+          if(squareMap[y][x] > 0 && squareMap[y][x] < 10){
+            squareMap[y+1][x] = squareMap[y][x]
+            squareMap[y][x] =0
+
+          }
+
+
+        }
+
+      }
+
+
+    }
+
+  }
+
+  function moveShapesDownKey(){
+    let canMove = true
+
+    for( let y = 0; y < squareMap.length; y++) {
+
+      for (let x=0; x<squareMap[y].length; x++) {
+
+        if(squareMap[y][x] > 0 && squareMap[y][x] < 10){
+          if(y === squareMap.length -2 || squareMap[y+2][x] >10 ) {
+            canMove = false
+
+          }
+
+        }
+      }
+
+    }
+    if(canMove) {
+
+      for(let y=squareMap.length-1;  y>=0;  y-- ){
+
+        for (let x=0; x<squareMap[y].length; x++) {
+          if(squareMap[y][x] > 0 && squareMap[y][x] < 10){
+            squareMap[y+1][x] = squareMap[y][x]
+            squareMap[y][x] =0
+
+          }
+
+
+        }
+
+      }
+
+
+    }
+
+  }
+
+
+
+  function freeze(){
+    for( let y = 0; y < squareMap.length; y++) {
+
+      for (let x=0; x<squareMap[y].length; x++) {
+
+        if(squareMap[y][x] > 0 && squareMap[y][x] < 10){
+          squareMap[y][x] = squareMap[y][x] + 10
+        }
+      }
+    }
+    checkLines()
+
+    let ran = Math.floor(Math.random()*7)
+
+    ran =5
+    if (ran === 0){
+
+
+      squareMap[0] =[0,0,0,4,4,0,0,0]
+
+      squareMap[1]= [0,0,0,4,4,0,0,0]
+
+
+    }else if (ran === 1){
+
+      squareMap[0] =[0,0,0,0,0,0,0,0]
+
+      squareMap[1]= [0,0,1,1,1,1,0,0]
+
+    }else if (ran === 2){
+
+      squareMap[0] =[0,0,0,2,0,0,0,0]
+
+      squareMap[1]= [0,0,0,2,2,2,0,0]
+    }else if (ran === 3){
+
+      squareMap[0] =[0,0,0,0,0,3,0,0]
+
+      squareMap[1]= [0,0,0,3,3,3,0,0]
+    }else if (ran === 4){
+
+      squareMap[0] =[0,0,0,0,5,5,0,0]
+
+      squareMap[1]= [0,0,0,5,5,0,0,0]
+    }else if (ran === 5){
+
+      squareMap[0] =[0,0,0,6,0,0,0,0]
+
+      squareMap[1]= [0,0,6,6,6,0,0,0]
+    }else if (ran === 6){
+
+      squareMap[0] =[0,0,7,7,0,0,0,0]
+
+      squareMap[1]= [0,0,0,7,7,0,0,0]
+    }
+  }
+
+
+
+  function moveShapesLeft(){
+    let canMove = true
+
+    for( let y = 0; y < squareMap.length; y++) {
+
+      for (let x=0; x<squareMap[y].length; x++) {
+
+        if(squareMap[y][x] > 0 && squareMap[y][x] < 10){
+          if(x === 0 || squareMap[y][x-1] >10 ) {
+            canMove = false
+
+          }
+
+        }
+      }
+
+    }
+    if(canMove) {
+
+      for(let y=squareMap.length-1;  y>=0;  y-- ){
+
+        for (let x=0; x<squareMap[y].length; x++) {
+          if(squareMap[y][x] > 0 && squareMap[y][x] < 10){
+            squareMap[y][x-1] = squareMap[y][x]
+            squareMap[y][x] =0
+
+          }
+
+        }
+
+      }
+
+    }
+
+  }
+
+
+
+  function moveShapesRight(){
+    let canMove = true
+
+    for( let y = 0; y < squareMap.length; y++) {
+
+      for (let x=0; x<squareMap[y].length; x++) {
+
+        if(squareMap[y][x] > 0 && squareMap[y][x] < 10){
+          if(x === 8 || squareMap[y][x+1] >10 ) {
+            canMove = false
+
+          }
+
+        }
+      }
+
+    }
+    if(canMove) {
+
+      for(let y=squareMap.length-1;  y>=0;  y-- ){
+
+        for (let x=squareMap[y].length; x>=0; x--) {
+          if(squareMap[y][x] > 0 && squareMap[y][x] < 10){
+            squareMap[y][x+1] = squareMap[y][x]
+            squareMap[y][x] =0
+
+          }
+
+
+        }
+
+      }
+
+
+    }
+
+  }
+
+
+
+  function rotateShape(){
+    let canMove = true
+
+    for( let y = 0; y < squareMap.length; y++) {
+
+      for (let x=0; x<squareMap[y].length; x++) {
+
+        if(squareMap[y][x] === 1){
+          if(x === 8 || squareMap[y][x+1] >10 ) {
+            canMove = false
+
+          }
+
+        }else if(squareMap[y][x] === 2){
+          if(x === 8 || squareMap[y][x+1] >10 ) {
+            canMove = false
+
+          }
+
+        } else if(squareMap[y][x] === 2){
+          if(x === 8 || squareMap[y][x+1] >10 ) {
+            canMove = false
+
+          }
+
+        } else if(squareMap[y][x] === 3){
+          if(x === 8 || squareMap[y][x+1] >10 ) {
+            canMove = false
+
+          }
+
+        } else if(squareMap[y][x] === 4){
+          if(x === 8 || squareMap[y][x+1] >10 ) {
+            canMove = false
+
+          }
+
+        } else if(squareMap[y][x] === 5){
+          if(x === 8 || squareMap[y][x+1] >10 ) {
+            canMove = false
+
+          }
+
+        } else if(squareMap[y][x] === 6){
+          if(x === 8 || squareMap[y][x+1] >10 ) {
+            canMove = false
+
+          }
+
+        } else if(squareMap[y][x] === 7){
+          if(x === 8 || squareMap[y][x+1] >10 ) {
+            canMove = false
+
+          }
+
+        }
+      }
+
+    }
+    if(canMove) {
+
+      for(let y=squareMap.length-1;  y>=0;  y-- ){
+
+        for (let x=squareMap[y].length; x>=0; x--) {
+          if(squareMap[y][x] !== 6 &&squareMap[y][x] > 0 && squareMap[y][x] < 10){
+            squareMap[y][x+1] = squareMap[y][x]
+            squareMap[y][x] =0
+
+// t Rotate
+          } if(squareMap[y][x] === 6 && squareMap[y][x+1] === 6 && squareMap[y][x+2] === 6 && squareMap[y-1][x+1] === 6 )
+          { squareMap[y][x] = 0
+            squareMap[y+1][x+1] = 6
+            squareMap[y-1][x+1] = 6
+            console.log('1')
+          }else if(squareMap[y][x] === 6 && squareMap[y][x+1] === 6 && squareMap[y+1][x] === 6 && squareMap[y-1][x] === 6 )
+          { squareMap[y-1][x] = 0
+            squareMap[y][x-1] = 6
+            squareMap[y][x+1] = 6
+            console.log('2')
+          }else if(squareMap[y][x] === 6 && squareMap[y][x+1] === 6 && squareMap[y+1][x] === 6 && squareMap[y][x-1] === 6 && squareMap[y-1][x] === 0)
+          { squareMap[y][x+1] = 0
+            squareMap[y][x-1] = 6
+            squareMap[y+1][x] = 6
+            squareMap[y-1][x] = 6
+            console.log('3')
+          } else if(squareMap[y][x] === 6 && squareMap[y][x-1] === 6 && squareMap[y+1][x] === 6 && squareMap[y-1][x] === 6){
+            squareMap[y+1][x] = 0
+            squareMap[y][x+1] = 6
+            squareMap[y-1][x] = 6
+            squareMap[y][x-1] = 6
+            console.log('4')
+            return
+          }
+
+
+
+
+        }
+
+      }
+
+
+    }
+
+  }
+
+
+  function checkLines(){
+
+    for( let y = 0; y < squareMap.length; y++) {
+      let  fullLine=true
+      for (let x=0; x<squareMap[y].length; x++) {
+        if(squareMap[y][x] < 10) {
+          fullLine= false
+        }
+
+
+      }
+      if (fullLine) {
+        squareMap.splice(y, 1)
+        squareMap.splice(0,0,[0,0,0,0,0,0,0,0,0])
+        y--
+        score++
+
+      }
+    }
+  }
+
+
+  document.onkeydown =function(e){
+    if(e.keyCode === 37){
+      moveShapesLeft()
+    } else if(e.keyCode === 39){
+      moveShapesRight()
+    } else if(e.keyCode === 40){
+      moveShapesDownKey()
+    } else if(e.keyCode === 38){
+      rotateShape()
+    }
+
+
+  }
+
+
+
+
+
+
+
+  squareMap[2] = iMap
+
+
+
   setInterval(function () {
 
 
-    squareMap[2] = iMap
-    console.log(squareMap)
+    //console.log(squareMap)
 
 
-
+    checkLines()
     drawWorld(1, 'i')
     drawWorld(2, 'l')
     drawWorld(3, 'j')
@@ -294,32 +576,32 @@ squareMap[1]= [0,0,0,8,8,8,0,0]
 
 
 
-  const pieces = [i, l, j, o, s, t, z ]
-  const piecesColor = ['i', 'l', 'j', 'o', 's', 't', 'z' ]
-  let choice = random
+  //const pieces = [i, l, j, o, s, t, z ]
+  //  piecesColor = ['i', 'l', 'j', 'o', 's', 't', 'z' ]
+  //const choice = random
 
-  let inPlay = pieces[choice]
-  let inPlayColor = piecesColor[choice]
-  console.log(inPlay)
+  //let inPlay = pieces[choice]
+  //const inPlayColor = piecesColor[choice]
+  //console.log(inPlay)
 
 
-const row1 = [squares[0],squares[1],squares[2], squares[3], squares[4], squares[5], squares[6], squares[7], squares[8]]
+  //const row1 = [squares[0],squares[1],squares[2], squares[3], squares[4], squares[5], squares[6], squares[7], squares[8]]
 
-const row2 = [squares[9],squares[10],squares[11], squares[12], squares[13], squares[14], squares[15], squares[16], squares[17]]
+  //const row2 = [squares[9],squares[10],squares[11], squares[12], squares[13], squares[14], squares[15], squares[16], squares[17]]
 
-const row3 = [squares[18],squares[19],squares[20], squares[21], squares[22], squares[23], squares[24], squares[25], squares[26]]
+  //const row3 = [squares[18],squares[19],squares[20], squares[21], squares[22], squares[23], squares[24], squares[25], squares[26]]
 
-const row4 = [squares[27],squares[28],squares[29], squares[30], squares[31], squares[32], squares[33], squares[34], squares[35]]
+  //const row4 = [squares[27],squares[28],squares[29], squares[30], squares[31], squares[32], squares[33], squares[34], squares[35]]
 
-const row5 = [squares[36],squares[37],squares[38], squares[39], squares[40], squares[41], squares[42], squares[43], squares[44]]
+  //const row5 = [squares[36],squares[37],squares[38], squares[39], squares[40], squares[41], squares[42], squares[43], squares[44]]
 
-const row6 = [squares[45],squares[46],squares[47], squares[48], squares[49], squares[50], squares[51], squares[52], squares[53]]
+  //const row6 = [squares[45],squares[46],squares[47], squares[48], squares[49], squares[50], squares[51], squares[52], squares[53]]
 
-const row7 = [squares[54],squares[55],squares[56], squares[57], squares[58], squares[59], squares[60], squares[61], squares[62]]
+  //const row7 = [squares[54],squares[55],squares[56], squares[57], squares[58], squares[59], squares[60], squares[61], squares[62]]
 
-const row8 = [squares[63],squares[64],squares[65], squares[66], squares[67], squares[68], squares[69], squares[70], squares[71]]
+  //const row8 = [squares[63],squares[64],squares[65], squares[66], squares[67], squares[68], squares[69], squares[70], squares[71]]
 
-const row9 = [squares[72],squares[73],squares[74], squares[75], squares[76], squares[77], squares[78], squares[79], squares[80]]
+  const row9 = [squares[72],squares[73],squares[74], squares[75], squares[76], squares[77], squares[78], squares[79], squares[80]]
 
   console.log(square)
 
@@ -332,12 +614,12 @@ const row9 = [squares[72],squares[73],squares[74], squares[75], squares[76], squ
 
   //inPlay.forEach( element  => {
 
-//    setInterval(function () {
+  //    setInterval(function () {
 
 
   //    squares[element].classList.add(inPlayColor)
   //    const time =  setTimeout(function () {
-        //if(element+ 9 <= 81 && squares[element].classList.contains('stopped') !== true){
+  //if(element+ 9 <= 81 && squares[element].classList.contains('stopped') !== true){
   //        squares[element].classList.remove(inPlayColor)
   //        element +=9
   //      } else if (element+ 9 >= 81 || (element +9).classList.contains('stopped')   ){
@@ -346,11 +628,11 @@ const row9 = [squares[72],squares[73],squares[74], squares[75], squares[76], squ
 
 
 
-    //    }
-    //  }, 1000)
+  //    }
+  //  }, 1000)
 
   //  }, 2000)
-//  })
+  //  })
 
   //console.log(i[0])
 
